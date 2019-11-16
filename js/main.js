81bba5fbad15df15a2ed1f33573661ce257af66b
UsmanRajput89 <usmanrana18989@gmail.com>
2019-11-16 23:02:00 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2019/12/01';
+        var finalDate = '2019/11/30';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
